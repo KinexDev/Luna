2795028c010881eec988cd2ac0700578cb51b272
DataDev <104727812+KinexDev@users.noreply.github.com>
2025-03-25 17:04:30 +0000

    first commit

---
+++ LuauBridge.cpp
@@ -0,0 +1,12 @@
+﻿// LuauBridge.cpp : Defines the entry point for the application.
+//
+
+#include "LuauBridge.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
